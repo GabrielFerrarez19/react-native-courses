23d7023c6dcef35e0698f6f3d23854210eb2fdef
GabrielFerrarez19 <gabrieferrarez190206@gmail.com>
2026-05-18 17:10:08 -0300

    aula 19

---
+++ comprar-app/src/app/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Image, View } from "react-native";
+
+import { styles } from "./styles";
+import { Button } from "@/components/Button/index";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require("@/assets/logo.png")} />
+      <Button title="Entrar" />
+    </View>
+  );
+}
