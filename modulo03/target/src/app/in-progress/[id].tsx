f521a28b3780701b01c7f1e4711ad4c5142c6fef
GabrielFerrarez19 <gabrieferrarez190206@gmail.com>
2026-05-25 17:23:40 -0300

    aula 84

---
+++ modulo03/target/src/app/in-progress/[id].tsx
@@ -0,0 +1,12 @@
+import { router, useLocalSearchParams } from "expo-router";
+import { View, Button, Text } from "react-native";
+
+export default function InProgress() {
+  const params = useLocalSearchParams<{ id: string }>();
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Id: {params.id}</Text>
+      <Button title="Voltar" onPress={() => router.back()} />
+    </View>
+  );
+}
